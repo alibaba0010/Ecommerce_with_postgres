4f867d34e7b11346a4db6f9e4dd6ccfdd911e4f2
alibaba0010 <zakariyahali100@gmail.com>
2024-10-21 07:07:36 +0100

    Adding new packages to the repository

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,14 @@
 import { createServer } from "http";
 import dotenv from "dotenv";
-import connectDB from "./db.js";
-import app from "./app.js";
+// import connectDB from "./db";
+import app from "./app";
 dotenv.config();
 const PORT = process.env.PORT || 2000;
 const uri = process.env.MONGO_URL;
 const server = createServer(app);
 
 (async () => {
-  await connectDB(uri);
+  // await connectDB(uri);
   server.listen(PORT, () =>
     console.log(`Listening to port @ http://localhost:${PORT}`)
   );
